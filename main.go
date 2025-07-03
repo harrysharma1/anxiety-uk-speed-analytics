8d950ec5556bab544c9ebb2fba48e11c2e5b26e2
Harry Sharma <harrysharma1066@gmail.com>
2025-07-03 15:26:38 +0100

    feat: add single page function

--- main.go
+++ main.go
@@ -48,7 +48,32 @@ func analyse_pages() {
 	}
 }
 
+func analyse_page(i int) {
+	const endpoint = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed`
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	key := os.Getenv("API_KEY")
+
+	urlList := read_urls()
+
+	fmt.Printf("Running analysis on url: %s...\n", urlList[i])
+	fullEndpoint := fmt.Sprintf("%s?url=%s&key=%s", endpoint, urlList[i], key)
+	fmt.Println(fullEndpoint)
+	resp, err := http.Get(fullEndpoint)
+	if err != nil {
+		panic(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+}
+
 func main() {
 	fmt.Printf("OS: %s\nArchitecture: %s\n", runtime.GOOS, runtime.GOARCH)
-	analyse_pages()
+	analyse_page(1)
 }
